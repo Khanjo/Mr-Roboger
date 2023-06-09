1b07f1aa75d6dede37a49dfe6d921ef70d6bdc24
Joshua Khan <Khanjo@gmail.com>
2023-06-09 09:03:52 -0700

    pass first test

---
+++ README.md
@@ -0,0 +1,9 @@
+Describe: roboOutput()
+
+Test: "It should return an array of numbers from 0 to the user's inputted number"
+Code: roboOutput(5);
+Expected Output: [0, 1, 2, 3, 4, 5]
+
+Test: "It should return any number with a 3 in it as 'won't you be my neighbor?'"
+Code: roboOutput(15)
+Expected Output: [0, 1, 2, "won't you be my neighbor?", 4, 5, 6, 7, 8, 9, 10, 11, 12, "won't you be my neighbor?", 14, 15]
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
+//Business Logic
+
+function roboOutput(number) {
+    let three = "won't you be my neighbor?"
+    let numArray = [];
+    for (let i = 0; i <= number; i++) {
+        if (i.toString().indexOf("3") !== -1) {
+            numArray.splice(i, 1, three);
+        } else {
+            numArray.push(i);
+        };
+    };
+    console.log(numArray)
+};
+
 //User Interface Logic
+
 window.addEventListener("load", function () {
 
 })
-
-//Business Logic
