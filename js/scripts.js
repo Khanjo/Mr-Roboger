ab90ba8375acd28ee79940e8dd1bed6096ce20b3
Joshua Khan <Khanjo@gmail.com>
2023-06-09 11:35:10 -0700

    pass fourth test

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,15 @@
 function roboOutput(number) {
     let three = "won't you be my neighbor?"
     let two = "boop"
+    let one = "beep"
     let numArray = [];
     for (let i = 0; i <= number; i++) {
         if (i.toString().indexOf("3") !== -1) {
             numArray.splice(i, 1, three);
         } else if (i.toString().indexOf("2") !== -1) {
             numArray.splice(i, 1, two);
+        } else if (i.toString().indexOf("1") !== -1) {
+            numArray.splice(i, 1, one);
         } else {
             numArray.push(i);
         }
